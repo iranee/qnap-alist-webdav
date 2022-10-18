8500821768cb883072d0c15b13ebd6b17395fce5
Alireza <bbis@cheen.cn>
2022-10-18 14:26:00 +0800

    update to v3.2.0

--- shared/alist-service.sh
+++ shared/alist-service.sh
@@ -2,21 +2,6 @@
 CONF=/etc/config/qpkg.conf
 QPKG_NAME="alist"
 QPKG_ROOT=`/sbin/getcfg $QPKG_NAME Install_Path -f ${CONF}`
-APACHE_ROOT=/share/`/sbin/getcfg SHARE_DEF defWeb -d Qweb -f /etc/config/def_share.info`
-
-export QNAP_QPKG=$QPKG_NAME
-export QPKG_ROOT
-export QPKG_NAME
-export APACHE_ROOT
-
-export HOME=$QPKG_ROOT
-export SHELL=/bin/sh
-export DESC=$QPKG_NAME
-
-if [ `/sbin/getcfg "QWEB" "Enable" -d 1` = 0 ]; then
-  echo "Web服务器尚未启用，请前往[控制台]→[应用程序]→[Web服务器]开启"
-  /sbin/log_tool  -N "Web服务器" -G "状态" -t1 -uSystem -p127.0.0.1 -mlocalhost -a "Web服务尚未启用，请前往[控制台]→[应用程序]→[Web服务器]开启，并重启[多云盘挂载]。"
-fi
 
 case "$1" in
   start)
@@ -25,10 +10,10 @@ case "$1" in
         echo "$QPKG_NAME 已禁用"
         exit 1
     fi
- 
-if [ ! -f "$QPKG_ROOT/alist" ]; then
-/sbin/log_tool -N "多云盘挂载" -G "Error" -t1 -uSystem -p127.0.0.1 -mlocalhost -a "[多云盘挂载] 启动文件alist丢失，请尝试重新安装插件。"
-fi
+     
+    if [ -f "$QPKG_ROOT/data/" ]; then
+    /sbin/log_tool -N "多云盘挂载" -G "Error" -t1 -uSystem -p127.0.0.1 -mlocalhost -a "[多云盘挂载] 启动文件alist丢失，请尝试重新安装插件。"
+    fi
 
 	/bin/chmod -Rf 777 $QPKG_ROOT/*
 	cd $QPKG_ROOT
