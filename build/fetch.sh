c0a2bea6725569cff130ecd65b17dc19ac16d466
Jiajun Chen <tychenjiajun@live.cn>
2023-02-07 20:52:08 +0800

    Update fetch.sh

--- build/fetch.sh
+++ build/fetch.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+source ../AList/qpkg.cfg
+
 mkdir -p out/tar
 mkdir -p out/extracted
 
 for ARCH in 386 amd64 arm64; do
     echo "Fetching alist_${ARCH}.tgz"
-    wget "https://github.com/alist-org/alist/releases/download/v3.9.2/alist-linux-${ARCH}.tar.gz" -O "out/tar/alist_${ARCH}.tgz"
+    wget "https://github.com/alist-org/alist/releases/download/v${QPKG_VER}/alist-linux-${ARCH}.tar.gz" -O "out/tar/alist_${ARCH}.tgz"
     tar xvzf "out/tar/alist_${ARCH}.tgz" -C "out/extracted"
     mv "out/extracted/alist" "out/alist-${ARCH}"
 done
