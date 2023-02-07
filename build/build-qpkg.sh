2e2e8f715186885034075d7eb8c6bca9a10ae087
Jiajun Chen <tychenjiajun@live.cn>
2023-02-07 13:10:42 +0000

    arm

--- build/build-qpkg.sh
+++ build/build-qpkg.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-mkdir -p ./AList/x86
-cp ./out/alist-386 ./AList/x86/alist
+mkdir -p ./AList/arm_x31
+cp ./out/alist-arm ./AList/arm_x31/alist
+
+mkdir -p ./AList/arm_x41
+cp ./out/alist-arm ./AList/arm_x41/alist
 
 mkdir -p ./AList/x86_64
 cp ./out/alist-amd64 ./AList/x86_64/alist
@@ -9,6 +12,7 @@ cp ./out/alist-amd64 ./AList/x86_64/alist
 mkdir -p ./AList/arm_64
 cp ./out/alist-arm64 ./AList/arm_64/alist
 
-/usr/share/QDK/bin/qbuild --root ./AList --build-arch x86 --build-dir ../
+/usr/share/QDK/bin/qbuild --root ./AList --build-arch arm_x31 --build-dir ../
+/usr/share/QDK/bin/qbuild --root ./AList --build-arch arm_x41 --build-dir ../
 /usr/share/QDK/bin/qbuild --root ./AList --build-arch x86_64 --build-dir ../
 /usr/share/QDK/bin/qbuild --root ./AList --build-arch arm_64 --build-dir ../
--- build/fetch.sh
+++ build/fetch.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 . ./AList/qpkg.cfg
-. AList/qpkg.cfg
-. ../AList/qpkg.cfg
 
 mkdir -p out/tar
 mkdir -p out/extracted
 
-for ARCH in 386 amd64 arm64; do
+for ARCH in amd64 arm64 arm; do
     echo "Fetching alist_${ARCH}.tgz"
-    wget "https://github.com/alist-org/alist/releases/download/v${QPKG_VER}/alist-linux-${ARCH}.tar.gz" -O "out/tar/alist_${ARCH}.tgz"
+    wget "https://github.com/alist-org/alist/releases/download/v${QPKG_VER}/alist-linux-musl-${ARCH}.tar.gz" -O "out/tar/alist_${ARCH}.tgz"
     tar xvzf "out/tar/alist_${ARCH}.tgz" -C "out/extracted"
     mv "out/extracted/alist" "out/alist-${ARCH}"
 done
